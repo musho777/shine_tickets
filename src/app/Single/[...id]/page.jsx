583f4b504bce75ae4b6b28006aad2cf45ab7c624
musho777 <129147695+musho777@users.noreply.github.com>
2024-05-23 01:00:10 +0400

    single page

---
+++ src/app/Single/[...id]/page.jsx
@@ -0,0 +1,127 @@
+"use client"
+import '../styles.css'
+import { useEffect, useState } from 'react'
+import { PuffLoader } from 'react-spinners'
+import { useParams, } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { useDispatch, useSelector } from 'react-redux'
+import { GetSinglPage } from '../../../services/action/action'
+import { TopEvents } from '../../../components/TopEvents/TopEvents'
+import { Card } from '../card'
+
+const Single = ({ params }) => {
+    const dispatch = useDispatch()
+    const id = params.id[0]
+    const { t } = useTranslation();
+    const getSinglPage = useSelector((st) => st.getSinglPage)
+    const { language } = useSelector((st) => st.StaticReducer)
+    let { event } = getSinglPage?.events
+    let { recomended } = getSinglPage?.events
+    var months = [
+        "January", "February", "March", "April", "May", "June",
+        "July", "August", "September", "October", "November", "December"
+    ];
+    var daysOfWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    const [languageData, setLanguageData] = useState({ title: '', description: '', hall: '', place: '' })
+    useEffect(() => {
+        dispatch(GetSinglPage(id))
+    }, [])
+    useEffect(() => {
+        let item = { ...languageData }
+        if (language === 'am') {
+            item.title = event?.title
+            item.description = event?.description
+            item.hall = event?.sessions[0]?.hallId.place
+            item.place = event?.sessions[0]?.hallId.hall
+        }
+        else if (language === 'en') {
+            item.title = event?.title_en
+            item.description = event?.description_en
+            item.hall = event?.sessions[0]?.hallId?.place_en
+            item.place = event?.sessions[0]?.hallId.hall_en
+        }
+        else if (language === 'ru') {
+            item.title = event?.title_ru
+            item.description = event?.description_ru
+            item.hall = event?.sessions[0]?.hallId?.place_ru
+            item.place = event?.sessions[0]?.hallId.hall_ru
+        }
+        setLanguageData(item)
+    }, [language, event])
+
+    if (getSinglPage.loading) {
+        return (
+            <div className='container'>
+                <div className='loading'>
+                    <PuffLoader color="#FEE827" />
+                </div>
+            </div>
+        )
+    }
+    return (
+        <div id='singlPage' className='container'>
+            <Card
+                time={event?.sessions[0]?.time}
+                img={`https://api.shinetickets.com/images/${getSinglPage.events.event?.image}`}
+                imgLarg={`https://api.shinetickets.com/images/${getSinglPage.events.event?.largeImage}`}
+                id={id}
+                data={event?.sessions[0]?.date}
+                description={languageData?.description}
+                title={languageData?.title}
+                priceEnd={`${event?.sessions[0]?.priceEnd} AMD`}
+                priceStart={`${event?.sessions[0]?.priceStart} -`}
+                hall={languageData?.hall}
+                place={languageData?.place}
+                onClick={() => window.location = `/BuyTickets/${id}`}
+                largImage={
+                    getSinglPage.events.event?.largeImage ? `https://api.shinetickets.com/images/${getSinglPage.events.event?.largeImage}` :
+                        `https://api.shinetickets.com/images/${getSinglPage.events.event?.image}`
+
+                }
+            />
+            {languageData?.description?.length > 0 && <div className='DescriptionDiv'>
+                <p className='descriptionDiv2Title'>{t('description')}</p>
+                <p>{languageData?.description}</p>
+            </div>}
+            <div className='RecDiv2'>
+                {
+                    recomended?.length > 0 &&
+                    <div className='EventTitle'>
+                        <h2>{t('RecommendTickets')}</h2>
+                        <div className='RecDiv'>
+                            {recomended.map((elm, i) => {
+                                const dateObject = new Date(elm.sessions[0]?.date);
+                                let day = dateObject.getDate();
+                                let month = dateObject.getMonth();
+                                var currentDayOfWeek = daysOfWeek[dateObject.getDay()];
+                                if (elm?.sessions?.length)
+                                    if (elm._id != "65d21c1851424e16acf107d4")
+                                        return <TopEvents
+                                            key={i}
+                                            id={elm?._id}
+                                            image={`https://api.shinetickets.com/images/${elm?.image}`}
+                                            title={elm?.title}
+                                            category={elm?.category}
+                                            day={day}
+                                            location={elm?.sessions[0]?.hallId?.location}
+                                            location_en={elm?.sessions[0]?.hallId?.location_en}
+                                            location_ru={elm?.sessions[0]?.hallId?.location_ru}
+                                            data={elm}
+                                            time={elm?.sessions[0]?.time}
+                                            months={months[month]}
+                                            currentDayOfWeek={currentDayOfWeek}
+                                            price={`${elm.sessions[0]?.priceStart} - ${elm.sessions[0]?.priceEnd} AMD`}
+                                        />
+                            })}
+                        </div>
+                        {/* <div className="ShowAllButtonWrappr">
+                            <ShowAllButton />
+                        </div> */}
+                    </div>
+                }
+            </div>
+        </div>
+    )
+}
+
+export default Single
---
+++ src/app/Single/card.jsx
@@ -0,0 +1,202 @@
+import { useTranslation } from 'react-i18next';
+import { Button } from '../../components/Button'
+import { LocationSvg, SeansCaelndar } from '../../components/svg'
+import { useEffect, useRef, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { ActiveSeans } from '../../services/action/action';
+
+export const Card = ({
+    id,
+    img,
+    largImage,
+    title,
+    description,
+    priceEnd,
+    priceStart,
+    hall,
+    time,
+    data,
+    onClick,
+    isParonyan,
+    seans,
+    setActiveSeans,
+    imgLarg,
+    place
+}) => {
+    const { t } = useTranslation();
+    const [data1, setData1] = useState(seans)
+    const [active, setActive] = useState(0)
+    const [showAll, setShowAll] = useState(false)
+    const divRef = useRef()
+    const [hight, setHeight] = useState(0)
+    const [openSeans, setOpenSeans] = useState(false)
+    const [date, setDate] = useState()
+    const dispatch = useDispatch()
+    document.body.addEventListener('click', function () {
+        setOpenSeans(false)
+    });
+
+    useEffect(() => {
+        if (divRef.current) {
+            const height = divRef.current.clientHeight;
+            setHeight(height);
+        }
+    }, [description]);
+
+    useEffect(() => {
+        if (data1?.length > 0) {
+            dispatch(ActiveSeans(data1[0].id))
+            setActiveSeans(data1[0].id)
+        }
+        let date = new Date(data)
+        let day = date.getDate()
+        let mount = date.getMonth() + 1
+        if (day < 10) {
+            day = `0${day}`
+        }
+        if (mount < 10) {
+            mount = `0${mount}`
+        }
+        setDate(`${day}-${mount}`)
+    }, [])
+
+    const [windowSize, setWindowSize] = useState({
+        width: window.innerWidth,
+        height: window.innerHeight
+    });
+
+    useEffect(() => {
+        const handleResize = () => {
+            setWindowSize({
+                width: window.innerWidth,
+                height: window.innerHeight
+            });
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
+    return <div style={{ height: hight }} className='SinglCaruselItem'>
+        <div ref={divRef} className='SinglBanerDiv' >
+            <div className='SiglBanerImg2'>
+                <img className='SiglBanerImg2' src={windowSize.width > 960 ? img : imgLarg} />
+                {data1?.length && <div className='SeansCaelndar'>
+                    <div onClick={(e) => {
+                        e.stopPropagation()
+                        e.preventDefault()
+                        setOpenSeans(true)
+                    }
+                    }>
+                        <SeansCaelndar />
+                    </div>
+                    {openSeans && <div className='SeansDivMobile'>
+                        {data1?.map((elm, i) => {
+                            const matchResult = elm.time.match(/(\d+)([\s\S]*?)(<div[\s\S]*?<\/div>)([\s\S]*?)(\d+:\d+)/);
+                            const day = matchResult[1];
+                            const divContent = matchResult[3];
+                            const parser = new DOMParser();
+                            const doc = parser.parseFromString(divContent, "text/html");
+                            const divElement = doc.body.firstChild;
+                            divElement.removeChild(divElement.querySelector('br'));
+                            const linesArray = Array.from(divElement.childNodes)
+                                .filter(node => node.nodeType === 3)
+                                .map(node => node.textContent.trim());
+                            const time = matchResult[5];
+                            return <div onClick={(e) => {
+                                e.stopPropagation()
+                                e.preventDefault()
+                                setActive(i)
+                                dispatch(ActiveSeans(elm.id))
+                                setActiveSeans(elm.id)
+
+                            }} className='SeansDivItem'>
+                                {i == active && <div className='SeansActive' />}
+                                {(linesArray[1].slice(0, 3) == 'ԿԻՐ' || linesArray[1].slice(0, 3) == 'ՇԱԲ') &&
+                                    <p style={{ color: 'red' }} className='WeekDey'>{linesArray[1].slice(0, 3)}</p>
+                                }
+                                <p className='WeekDey'>{linesArray[1].slice(0, 3)}</p>
+                                <p>{day} {linesArray[0]}</p>
+                                <p className='WeekDey'>{time}</p>
+                            </div>
+                        })}
+                    </div>}
+                </div>}
+            </div>
+            <div className='SinglBanerDivInfo'>
+                <div className='SinglBanerPrimera'>
+                    <div className='SinglPrimera'>
+                        <p className='SinglPrimerap'>{t('Primera')}</p>
+                        {isParonyan ?
+                            <p id="paronyan" className='SinglPrimeraDate' dangerouslySetInnerHTML={{ __html: data }} /> :
+                            <p className='SinglPrimeraDate'>{date} {time}</p>
+                        }
+                    </div>
+                    <div className='SinglBanerLocation'>
+                        <LocationSvg />
+                        <p className='SinglBanerDivInfoPlace'>{hall} {place}</p>
+                    </div>
+                </div>
+                <div>
+                    <p className='SinglBanerTitle'>{title}</p>
+                </div>
+                <div className='SinglBanerPrimeraMobile'>
+                    <div className='Primera'>
+                        <p className='Primerap'>{t('Primera')}</p>
+                        {isParonyan ?
+                            <p id="paronyan" className='PrimeraDate' dangerouslySetInnerHTML={{ __html: data }} /> :
+                            <p className='PrimeraDate'>{date} {time}</p>
+                        }
+                    </div>
+                    <div className='BanerLocation'>
+                        <LocationSvg />
+                        <p className='BanerDivInfoPlace'>{hall}</p>
+                    </div>
+                </div>
+                <div className='SinglPriceDiv'>
+                    <p className='SinglBanerPrice'>{priceStart} {priceEnd} </p>
+                    <div className='SinglBanerButton'>
+                        <Button
+                            onClick={onClick}
+                            title={t('BuyNow')} />
+                    </div>
+                </div>
+
+            </div>
+            {data1?.length > 0 && <div className='SeansDiv'>
+                {data1?.map((elm, i) => {
+                    const matchResult = elm.time.match(/(\d+)([\s\S]*?)(<div[\s\S]*?<\/div>)([\s\S]*?)(\d+:\d+)/);
+                    const day = matchResult[1];
+                    const divContent = matchResult[3];
+                    const parser = new DOMParser();
+                    const doc = parser.parseFromString(divContent, "text/html");
+                    const divElement = doc.body.firstChild;
+                    divElement.removeChild(divElement.querySelector('br'));
+                    const linesArray = Array.from(divElement.childNodes)
+                        .filter(node => node.nodeType === 3)
+                        .map(node => node.textContent.trim());
+                    const time = matchResult[5];
+                    return <div onClick={() => {
+                        setActive(i)
+                        dispatch(ActiveSeans(elm.id))
+                        setActiveSeans(elm.id)
+                    }} id={active == i ? 'SeansDivItemActive' : ''} className='SeansDivItem'>
+                        {i == active && <div className='SeansActive' />}
+                        {(linesArray[1].slice(0, 3) == 'ԿԻՐ' || linesArray[1].slice(0, 3) == 'ՇԱԲ') &&
+                            <p style={{ color: 'red' }} className='WeekDey'>{linesArray[1].slice(0, 3)}</p>
+                        }
+                        <p className='WeekDey'>{linesArray[1].slice(0, 3)}</p>
+                        <p>{day} {linesArray[0]}</p>
+                        <p className='WeekDey'>{time}</p>
+                    </div>
+                })}
+            </div>}
+        </div>
+        <img
+            className='SinglBanerImg'
+            src={largImage}
+            alt='#'
+        />
+    </div>
+}
